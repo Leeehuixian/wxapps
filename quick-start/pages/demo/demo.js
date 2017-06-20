e5f52274b4bdb8c196b77cc92fc74e91cf047226
huixian <huixian>
2017-06-20 08:02:37 +0800

    scroll-view学习，存留一个问题，scroll-x无效

--- quick-start/pages/demo/demo.js
+++ quick-start/pages/demo/demo.js
@@ -1,4 +1,5 @@
 // demo.js
+var order=["red","yellow","blue","green","red"]
 Page({
 
   /**
@@ -13,6 +14,24 @@ Page({
   },
   lower:function(e){
     console.log(e);
+  },
+  scroll:function(e){
+    console.log(e);
+  },
+  tap:function(e){
+    for(var i=0;i<order.length;i++){
+      if(order[i]===this.data.toView){
+        this.setData({
+          toView:order[i+1]
+        })
+        break
+      }
+    }
+  },
+  tapMove:function(e){
+    this.setData({
+      scrollTop:this.data.scrollTop + 10
+    })
   },
 
   /**
