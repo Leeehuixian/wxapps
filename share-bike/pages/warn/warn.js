bb8a76f39e478f96f1cd3bdffa45ce14e67b7fc4
lihuixian <lihuixina@jingui.com>
2017-07-14 18:36:17 +0800

    完成报修页面，新建picker页

--- share-bike/pages/warn/warn.js
+++ share-bike/pages/warn/warn.js
@@ -5,6 +5,10 @@ Page({
    * 页面的初始数据
    */
   data: {
+    checkboxValue:[],//故障类型数组
+    btnBgc:'',//提交按钮的景色，未勾选类型时无颜色
+    picUrls:[],//故障车周围环境路径数组
+    actionText:"拍照/照相",
     itemsValue:[
       {
         checked:false,
@@ -40,7 +44,81 @@ Page({
       }
     ],
   },
-
+  //勾选故障类型，获取类型值存入checkboxValue
+  checkboxChange: function(e){
+    let _values = e.detail.value;
+    if(_values.length == 0){
+      this.setData({
+        checkboxValue:[],
+        btnBgc:''
+      })
+    }else{
+      this.setData({
+        checkboxValue:_values,
+        btnBgc:"limegreen"
+      })
+    }
+  },
+  //选择故障车周围环境图拍照或选择相册
+  bindCamera: function () {
+    wx.chooseImage({
+      count:4,
+      sizeType:['original','compressed'],
+      sourceType:['album','camera'],
+      success:(res)=>{
+        let tfps = res.tempFilePaths;
+        let _picUrls = this.data.picUrls;
+        for(let item of tfps){
+          _picUrls.push(item);
+          this.setData({
+            picUrls:_picUrls,
+            actionText:"+"
+          });
+        }
+      }
+    })
+  },
+  //删除故障车周围环境图
+  delPic:function(e){
+    let index = e.target.dataset.index;
+    let _picUrls = this.data.picUrls;
+    _picUrls.splice(index,1);
+    this.setData({
+      picUrls:_picUrls
+    })
+  },
+  //提交到服务器
+  formSubmit:function(e){
+    if(this.data.checkboxValue.length >0){
+      wx.showLoading({
+        title: '上传服务器...'
+      });
+      setTimeout(function(){
+        wx.hideLoading();
+        wx.showToast({
+          title: '提交成功'
+        });
+      },1000);
+      setTimeout(function () {
+        wx.hideToast();
+      }, 2000);
+    }else{
+      wx.showModal({
+        title:"提示",
+        content:"请选择故障原因！",
+        confirmText:'确定',
+        cancelText:'取消',
+        success:(res)=>{
+          if(res.confirm){
+          }else{
+            wx.navigateBack({
+              delta:1//回退前detal（默认为1）页面
+            })
+          }
+        }
+      })
+    }
+  },
   /**
    * 生命周期函数--监听页面加载
    */
